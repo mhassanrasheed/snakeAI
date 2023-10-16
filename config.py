dec999a3569d52b90db6d84780c05f0c9aa66324
Hassan Rasheed <m.hassanrasheed95@gmail.com>
2023-10-16 19:40:32 +0100

    Code refactor: config file added

---
+++ config.py
@@ -0,0 +1,18 @@
+red = (255, 0, 0)
+green = (0, 255, 0)
+yellow = (255, 255, 102)
+width = 800
+height = 600
+speed = 10
+yellow = (255, 255, 102)
+population = 10
+gen = 0
+is_human = False
+is_learning = True
+resume_learning = False
+number_of_brains_from_previous_run = 10
+if is_human:
+    is_learning = False
+code_debugging = True
+prev_folder = "training/day6"
+to_save_folder = "training/day10"
